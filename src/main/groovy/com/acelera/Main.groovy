3f37a0453066fc4b766ca6a89ed2b828b678a1f9
Paloma Eduarda <palomaaeduardapro23@gmail.com>
2024-10-20 00:58:05 -0300

    Criando taks para baixar tabela com erros

---
+++ src/main/groovy/com/acelera/Main.groovy
@@ -0,0 +1,110 @@
+package com.acelera
+
+
+import org.jsoup.nodes.Document
+
+import static groovyx.net.http.HttpBuilder.configure
+
+static void main(String[] args) {
+
+
+    Document page = configure {
+        request.uri = 'https://www.gov.br/ans/pt-br'
+    }.get()
+
+    acessarCampoPrestaServico(page)
+
+
+
+}
+def acessarCampoPrestaServico(Document document) {
+
+    def prestadorDeServicos = document.getElementsContainingOwnText("Espaço do Prestador de Serviços de Saúde")
+
+    if (prestadorDeServicos) {
+        def linkElement = prestadorDeServicos.first().select("a").first()
+
+        if (linkElement != null) {
+            String prestadorUrl = linkElement.absUrl('href')
+            println "Link para Espaço do Prestador de Serviços de Saúde: ${prestadorUrl}"
+            acessarCampoTISS(prestadorUrl)
+        } else {
+            println "O link para Espaço do Prestador de Serviços de Saúde não foi encontrado."
+        }
+    } else {
+        println "O link para espaço do prestador não foi encontrado."
+    }
+}
+def acessarCampoTISS(prestador){
+
+    Document paginaPrestador = configure {
+        request.uri = prestador
+    }.get()
+
+    String urlTISS
+
+    def CampoTISS = paginaPrestador.getElementsContainingOwnText("TISS - Padrão para Troca de Informação de Saúde Suplementar")
+
+    if (CampoTISS) {
+        def linkElement = CampoTISS.first().select("a").first()
+
+        if(linkElement){
+            urlTISS =linkElement.absUrl('href')
+            println "Link para Campo TISS ${urlTISS} "
+            //acessarCampoPTSetembro(urlTISS)
+            acessarCampoTabelaRelacionada(urlTISS)
+        }else{
+            println "Erro"
+        }
+    } else {
+        println "O novo campo não foi encontrado na página."
+    }
+}
+def acessarCampoTabelaRelacionada(urlTISS){
+    TabelaErrosANS tabelaErrosANS = new TabelaErrosANS()
+
+    Document paginaTISS = configure {
+        request.uri = urlTISS
+    }.get()
+
+    String urlTb
+
+    def CampoTBrelacionada = paginaTISS.getElementsContainingOwnText("Clique aqui para acessar as planilhas")
+
+    if (CampoTBrelacionada) {
+        def linkElement = CampoTBrelacionada.first().select("a").first()
+        if(linkElement){
+            urlTb = linkElement.absUrl('href')
+            println "Link para tabelas relacionadas ${urlTb} "
+           tabelaErrosANS.acessarCampoTabela(urlTb)
+
+        }else{
+            println "Erro"
+        }
+    } else {
+        println "O novo campo não foi encontrado na página."
+    }
+}
+def acessarCampoPTSetembro(urlTISS){
+
+    Document paginaTISS = configure {
+        request.uri = urlTISS
+    }.get()
+
+    String urlPT
+
+    def CampoPTSet = paginaTISS.getElementsContainingOwnText("Clique aqui para acessar a versão Setembro/2024")
+
+    if (CampoPTSet) {
+        def linkElement = CampoPTSet.first().select("a").first()
+
+        if(linkElement){
+            urlPT = linkElement.absUrl('href')
+            println "Link para Campo Padrão TISS – Versão Setembro/2024 ${urlPT} "
+        }else{
+            println "Erro"
+        }
+    } else {
+        println "O novo campo não foi encontrado na página."
+    }
+}
---
+++ src/main/groovy/com/acelera/TabelaErrosANS.groovy
@@ -0,0 +1,47 @@
+package com.acelera
+
+import groovyx.net.http.optional.Download
+import org.jsoup.nodes.Document
+
+import static groovyx.net.http.HttpBuilder.configure
+
+class TabelaErrosANS {
+
+    void acessarCampoTabela(urlTabelaRelacionada){
+
+        Document paginaTabela = configure {
+            request.uri = urlTabelaRelacionada
+        }.get()
+
+
+        def CampoTB = paginaTabela.getElementsContainingOwnText("Clique aqui para baixar a tabela de erros no envio para a ANS (.xlsx)")
+
+        if (CampoTB) {
+            def linkElement = CampoTB.first().select("a").first()
+            if(linkElement){
+                String urlTb = linkElement.absUrl('href')
+                println "Link para tabelas relacionadas ${urlTb} "
+                baixarTabelaErrosANS(urlTb)
+
+            }else{
+                println "Erro"
+            }
+        } else {
+            println "O novo campo não foi encontrado na página."
+        }
+    }
+
+
+
+    void baixarTabelaErrosANS(urlTabelaRelacionada){
+        File saved = new File('/home/paloma/IdeaProjects/Projeto_Web_Crawler/recursos/TabelaErros.xlsx')
+
+        configure {
+            request.uri = urlTabelaRelacionada
+        }.get {
+            Download.toFile(delegate, saved)
+        }
+    }
+
+}
+//https://www.gov.br/ans/pt-br/arquivos/assuntos/prestadores/padrao-para-troca-de-informacao-de-saude-suplementar-tiss/padrao-tiss-tabelas-relacionadas/
